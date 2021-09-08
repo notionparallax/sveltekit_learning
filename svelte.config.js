85ff6e311c528cd3a00e9b5cc5514f5a9bc4fd7f
Ben <ben@notionparallax.co.uk>
2021-09-08 18:20:17 +1000

    🤯

---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+/** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-static';
+
+export default {
+	kit: {
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
+	}
+};
+
+// export default config;

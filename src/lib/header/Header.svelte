64958784fbb1ad4f67da61015a10c1029932462e
Ben <ben@notionparallax.co.uk>
2021-09-08 19:20:47 +1000

    add base to home

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -16,7 +16,9 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
+			<li class:active={$page.path === '/'}>
+				<a sveltekit:prefetch href="{base}/">Home</a>
+			</li>
 			<li class:active={$page.path === '/about'}>
 				<a sveltekit:prefetch href="{base}/about">About</a>
 			</li>

4f65e6005a9259e2f800ff33771d6b35e7e3c2c4
Ben <ben@notionparallax.co.uk>
2021-09-08 18:24:03 +1000

    point the build at docs

--- svelte.config.js
+++ svelte.config.js
@@ -5,8 +5,8 @@ export default {
 	kit: {
 		adapter: adapter({
 			// default options are shown
-			pages: 'build',
-			assets: 'build',
+			pages: 'docs',
+			assets: 'docs',
 			fallback: null
 		})
 	}

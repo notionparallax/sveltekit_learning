b7d84c190dd6ee22b2a0ef86939e2f27e19d5a21
Ben <ben@notionparallax.co.uk>
2021-09-10 12:18:29 +1000

    lorem component and more fiddling

---
+++ src/lib/LoremP.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan, diam et elementum
+	gravida, arcu mi fermentum nibh, vel dapibus ligula orci non est. Morbi commodo sagittis finibus.
+	Maecenas in volutpat massa. Nullam vulputate metus velit, quis interdum elit imperdiet ut.
+	Suspendisse et sagittis erat, euismod vulputate enim. Etiam feugiat sit amet justo vitae commodo.
+</p>
+
+<style></style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <script>
-	import Counter from '$lib/Counter.svelte';
+	import LoremP from '$lib/LoremP.svelte';
 
 	import {
 		MaterialApp,
@@ -13,9 +13,7 @@
 		Switch,
 		Tabs,
 		Tab,
-		Window,
-		WindowItem,
-		AppBar,
+		TabContent,
 		Icon
 	} from 'svelte-materialify';
 	//Icons: get list from https://materialdesignicons.com/
@@ -25,7 +23,6 @@
 	let slider_min = 10;
 	let slider_max = 50;
 	const values = [false, true];
-	let windowValue = 0;
 </script>
 
 <svelte:head>
@@ -33,84 +30,53 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
+	<h2>Debug</h2>
+	<dl>
+		<dt />
+		<dd />
+	</dl>
 </section>
 
 <section>
 	<MaterialApp {theme}>
-		<Button>Hello Annisa!</Button>
-		<Chip close>design</Chip><Chip close>underpants</Chip><Chip close>UX</Chip>
-		<Slider
-			value={[slider_min, slider_max]}
-			thumb={[true, true]}
-			persistentThumb
-			min={3}
-			max={100}
-			step={1}>Article range</Slider
-		>
-		<Switch bind:checked={values[0]} inset>Inset {values[0]}</Switch>
-		<Switch bind:checked={values[1]} inset>Inset {values[1]}</Switch>
-
-		<AppBar>
-			<span slot="title"> Title </span>
-			<div slot="extension">
-				<Tabs class="green-text" bind:windowValue fixedTabs>
-					<div slot="tabs">
-						<Tab><Icon class="primary-text" path={mdiHome} /><br />Item 1</Tab>
-						<Tab><Icon class="primary-text" path={mdiAxe} /><br />Item 2</Tab>
-						<Tab><Icon class="primary-text" path={mdiBathtub} /><br />Item 3</Tab>
-					</div>
-				</Tabs>
+		<Tabs centerActive>
+			<div slot="tabs">
+				<Tab>Home<Icon path={mdiHome} /></Tab>
+				<Tab>Address<Icon path={mdiAxe} /></Tab>
+				<Tab>Settings<Icon path={mdiBathtub} /></Tab>
 			</div>
-		</AppBar>
 
-		<Window {windowValue} class="ma-4">
-			<WindowItem>
-				<h4>Thing 1</h4>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan, diam et elementum
-					gravida, arcu mi fermentum nibh, vel dapibus ligula orci non est. Morbi commodo sagittis
-					finibus. Maecenas in volutpat massa. Nullam vulputate metus velit, quis interdum elit
-					imperdiet ut. Suspendisse et sagittis erat, euismod vulputate enim. Etiam feugiat sit amet
-					justo vitae commodo.
-				</p>
-			</WindowItem>
-			<WindowItem>
-				<h4>Item 2</h4>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan, diam et elementum
-					gravida, arcu mi fermentum nibh, vel dapibus ligula orci non est. Morbi commodo sagittis
-					finibus. Maecenas in volutpat massa. Nullam vulputate metus velit, quis interdum elit
-					imperdiet ut. Suspendisse et sagittis erat, euismod vulputate enim. Etiam feugiat sit amet
-					justo vitae commodo.
-				</p>
-			</WindowItem>
-			<WindowItem>
-				<h4>Item 3</h4>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec accumsan, diam et elementum
-					gravida, arcu mi fermentum nibh, vel dapibus ligula orci non est. Morbi commodo sagittis
-					finibus. Maecenas in volutpat massa. Nullam vulputate metus velit, quis interdum elit
-					imperdiet ut. Suspendisse et sagittis erat, euismod vulputate enim. Etiam feugiat sit amet
-					justo vitae commodo.
-				</p>
-			</WindowItem>
-		</Window>
+			<TabContent>
+				<h4>Home</h4>
+				<Button
+					on:click={() => {
+						alert("I knew you'd click this!");
+					}}>Hello Annisa!</Button
+				>
+				<LoremP />
+			</TabContent>
+			<TabContent>
+				<h4>Address</h4>
+				<LoremP />
+			</TabContent>
+			<TabContent>
+				<h4>Settings</h4>
+				<!-- <Slider
+					value={[slider_min, slider_max]}
+					thumb={[true, true]}
+					persistentThumb
+					min={3}
+					max={100}
+					step={1}>Article range</Slider
+				> -->
+				<Slider value={[10, 20]} thumb={[false, true]} persistentThumb>Range</Slider>
+				<Chip close>design</Chip><Chip close>underpants</Chip><Chip close>UX</Chip>
+				<LoremP />
+				<Switch bind:checked={values[0]} inset>Inset {values[0]}</Switch>
+				<Switch bind:checked={values[1]} inset>Inset {values[1]}</Switch>
+				<LoremP />
+			</TabContent>
+		</Tabs>
 	</MaterialApp>
 </section>
 
@@ -121,24 +87,5 @@
 		justify-content: center;
 		align-items: center;
 		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
